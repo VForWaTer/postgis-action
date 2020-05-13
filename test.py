f78d43c970895db28dbfad036b80327f3427d495
Mirko Mälicke <mirko.maelicke@kit.edu>
2020-05-13 19:26:47 +0200

    commit before create

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ print('[PORT] %s' % port)
 engine = create_engine('postgresql://postgres:postgres@localhost:%s/postgres' % port)
 
 with engine.connect() as connection:
+    connection.execute('commit')
     connection.execute('Create database metacatalog;')
 
 engine = create_engine('postgresql://postgres:postgres@localhost:%s/metacatalog' % port)
